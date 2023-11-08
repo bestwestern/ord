d855b5f22f43aa93929c79dfecb8ecebd146aad3
bestwestern <runjep@gmail.com>
2023-11-08 16:03:51 +0100

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable Preact to support Preact JSX components.
-	integrations: [preact()],
+	integrations: [preact()],svelte,
 });
