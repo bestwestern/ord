93448e3a5785217982c0d2c47ae609e44f780f73
bestwestern <runjep@gmail.com>
2023-11-08 16:11:54 +0100

    ffs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import svelte from '@astrojs/svelte'
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable Preact to support Preact JSX components.
-	integrations: [preact()],svelte,
+	// Enable Svelte to support Svelte components.
+	integrations: [svelte()],
 });
